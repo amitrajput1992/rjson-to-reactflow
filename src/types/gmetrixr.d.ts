5d0089e2893c4a435f114da647ea8ab9f425696f
Amit Rajput <amitrajput123.ar@gmail.com>
2025-03-27 19:38:13 +0530

    Fixing vite build

---
+++ src/types/gmetrixr.d.ts
@@ -0,0 +1,11 @@
+declare module '@gmetrixr/project-rjson/*' {
+  // This declaration file is used to suppress TypeScript errors from the @gmetrixr/project-rjson package
+  const content: any;
+  export = content;
+}
+
+declare module '@gmetrixr/gdash/*' {
+  // This declaration file is used to suppress TypeScript errors from the @gmetrixr/gdash package
+  const content: any;
+  export = content;
+}
